318c2519bd2dbb2dde6bac87275d5ae011ed478f
Rommaash <Rommaash@yandex.ru>
2024-10-25 20:32:53 +0300

    1 review

---
+++ Image Feed/ImageList/ImagesListCell.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    @IBOutlet var cellImage: UIImageView!
+    
+    @IBOutlet var likeButton: UIButton!
+    
+    @IBOutlet var dateLabel: UILabel!
+    static let reuseIdentifier = "ImagesListCell"
+}
