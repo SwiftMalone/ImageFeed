48e1ab6bbd60bbb04e4f32e63091b05f3cf190c2
Rommaash <Rommaash@yandex.ru>
2024-10-31 17:31:24 +0300

    1 review

--- Image Feed/ImageList/ImagesListViewController.swift
+++ Image Feed/ImageList/ImagesListViewController.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 final class ImagesListViewController: UIViewController {
-
+    private let showSingleImageSegueIdentifier = "ShowSingleImage"
     @IBOutlet private var tableView: UITableView!
     private let photosName: [String] = Array(0..<20).map{ "\($0)" }
     private let currentDate = Date()
@@ -32,6 +32,23 @@ final class ImagesListViewController: UIViewController {
         let likeImage = isLiked ? UIImage(named: "like_button_on") : UIImage(named: "like_button_off")
         cell.likeButton.setImage(likeImage, for: .normal)
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == showSingleImageSegueIdentifier {
+            guard
+                let viewController = segue.destination as? SingleImageViewController,
+                let indexPath = sender as? IndexPath
+            else {
+                assertionFailure("Invalid segue destination")
+                return
+            }
+            
+            let image = UIImage(named: photosName[indexPath.row])
+            viewController.image = image
+        } else {
+            super.prepare(for: segue, sender: sender)
+        }
+    }
 }
 
 extension ImagesListViewController: UITableViewDataSource {
@@ -52,8 +69,8 @@ extension ImagesListViewController: UITableViewDataSource {
 }
 extension ImagesListViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        // TODO: - Добавить логику при нажатии на ячейку
-    }
+          performSegue(withIdentifier: showSingleImageSegueIdentifier, sender: indexPath)
+      }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         guard let image = UIImage(named: photosName[indexPath.row]) else {
@@ -68,3 +85,5 @@ extension ImagesListViewController: UITableViewDelegate {
         return cellHeight
     }
 }
+
+
